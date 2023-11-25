7728d4fcc5bed38445d49b4cd217b5d34a28d414
devopsdbroe <dbroe1995@gmail.com>
2023-11-24 23:42:45 -0500

    Made Header responsive

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import logo from "../assets/Images/logo.png";
-import { IoSearch } from "react-icons/io5";
+import {
+	IoChevronDownOutline,
+	IoChevronUpOutline,
+	IoSearch,
+} from "react-icons/io5";
 
 const Header = () => {
+	const [toggle, setToggle] = useState(false);
+
 	const menu = [
 		{
 			id: 1,
@@ -36,7 +43,7 @@ const Header = () => {
 	return (
 		<div className="flex justify-between items-center py-4 px-10">
 			<img src={logo} alt="logo" className="w-[65px]" />
-			<ul className="flex gap-8">
+			<ul className="hidden md:flex gap-8">
 				{menu.map((item) => (
 					<li
 						key={item.id}
@@ -46,6 +53,28 @@ const Header = () => {
 					</li>
 				))}
 			</ul>
+			<div onClick={() => setToggle(!toggle)} className="md:hidden">
+				<h2 className="text-white font-medium flex items-center gap-2 px-3 py-2 bg-gray-700 rounded-md cursor-pointer">
+					Home
+					{!toggle ? (
+						<IoChevronDownOutline className="mt-1" />
+					) : (
+						<IoChevronUpOutline className="mt-1" />
+					)}
+				</h2>
+				{toggle && (
+					<ul className="absolute bg-gray-700 w-[200px] text-center mt-3 right-0 left-0 mx-auto rounded-md px-3">
+						{menu.map((item) => (
+							<li
+								key={item.id}
+								className="text-gray-400 text-[18px] font-medium cursor-pointer hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md transition-all duration-500 ease-in-out"
+							>
+								{item.name}
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
 			<div className="flex gap-10">
 				<IoSearch className="text-[35px] text-gray-300 hover:bg-gray-700 hover:text-white px-[3px] py-[2px] cursor-pointer rounded-md transition-all duration-500 ease-in-out" />
 				<h2 className="px-[10px] text-[20px] text-gray-300 border-[2px] border-white rounded-full">
