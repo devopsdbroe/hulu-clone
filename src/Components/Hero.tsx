a972e94c48e9d5d6daad7736b25075baa1ba5ca5
devopsdbroe <dbroe1995@gmail.com>
2023-11-25 13:55:40 -0500

    Fully implemented Hero section

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,20 +2,46 @@ import { useEffect, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
 
 const Hero = () => {
-	const [movieList, setMovieList] = useState([]);
+	const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+	const [movieList, setMovieList] = useState<any>([]);
 
 	useEffect(() => {
 		const getPopularMovies = () => {
 			GlobalApi.getPopularMovies.then((res: any) => {
 				const result = res.data.results;
-				setMovieList(result);
+				setMovieList(result[0]);
 			});
 		};
 
 		getPopularMovies();
 	}, []);
 
-	return <div>Hero</div>;
+	return (
+		<div>
+			<div className="absolute h-[85vh] bg-gradient-to-t from-[#1e2126] via-transparent to-transparent w-full"></div>
+			<div className="absolute mt-[400px] md:mt-[350px] px-10 md:px-24">
+				<h2 className="text-white text-[19px] lg:text-[27px]">
+					Watch only on Hulu
+				</h2>
+				<h2 className="text-white text-[36px] lg:text-[47px] font-bold">
+					{movieList.original_title}
+				</h2>
+				<div className="flex gap-5 mt-5">
+					<button>Play</button>
+					<button className="bg-transparent border-2 border-white text-white cursor-pointer hover:border-gray-500">
+						Details
+					</button>
+				</div>
+			</div>
+			<img
+				src={IMAGE_BASE_URL + movieList.backdrop_path}
+				alt="movie poster"
+				width={1920}
+				height={1080}
+				className="h-[85vh] object-cover"
+			/>
+		</div>
+	);
 };
 
 export default Hero;
